250f5a79a3d43c32027507d380fb7c17974ec493
MountainCat <tomasz.banas123@gmail.com>
2022-07-31 03:08:21 +0200

    small comment

--- Backend/TodoApi/Todos.Service/ServiceAssemblyPointer.cs
+++ Backend/TodoApi/Todos.Service/ServiceAssemblyPointer.cs
@@ -2,7 +2,7 @@
 
 
 // Class used to point to the Todos.Service assembly
-// Right now used only by MediaR
+// Right now used only by MediaR, and Fluent Validation
 public static class ServiceAssemblyPointer
 {
     // Intentionally empty...
