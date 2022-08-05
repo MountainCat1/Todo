68b46cc2584f3bf0c7d68bcc141d866d3235f5c0
MountainCat <tomasz.banas123@gmail.com>
2022-08-05 13:56:17 +0200

    added ITeamRepository.cs

---
+++ Backend/TeamsApi/Teams.Domain/Repositories/ITeamRepository.cs
@@ -0,0 +1,13 @@
+﻿using Teams.Domain.Entities;
+
+namespace Teams.Domain.Repositories;
+
+public interface ITeamRepository
+{
+    public Task<ICollection<Team>> GetAllAsync(Guid? teamGuid, Guid? userGuid);
+    public Task<ICollection<Team>> GetAllAsync();
+    public Task<Team?> GetAsync(Guid guid);
+    public Task<Team> CreateAsync(Team todo);
+    public Task DeleteAsync(Guid guid);
+    public Task<Team> UpdateAsync(Guid guid, Team todo);
+}
