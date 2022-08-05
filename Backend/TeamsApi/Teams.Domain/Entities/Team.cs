e313b82e6d50a732c48c149ec9f835592282c042
MountainCat <tomasz.banas123@gmail.com>
2022-08-05 13:54:31 +0200

    added key attribute to team entity

--- Backend/TeamsApi/Teams.Domain/Entities/Team.cs
+++ Backend/TeamsApi/Teams.Domain/Entities/Team.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Teams.Domain.Entities;
 
 public class Team
 {
+    [Key]
     public Guid Guid { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
