eda7d5ce8254fb1abe797277fa3f7ee5baae2564
MountainCat <tomasz.banas123@gmail.com>
2022-08-05 15:29:10 +0200

    added first query handler

---
+++ Backend/TeamsApi/Teams.Infrastructure/Repositories/TeamMemberRepository.cs
@@ -0,0 +1,27 @@
+﻿using Teams.Domain.Entities;
+using Teams.Domain.Repositories;
+
+namespace Teams.Infrastructure.Repositories;
+
+public class TeamMemberRepository : ITeamMemberRepository
+{
+    public async Task<ICollection<Guid>> GetMembersAsync(Guid teamGuid)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Team> AddMember(Guid userGuid, Guid teamGuid)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteMember(Guid userGuid, Guid teamGuid)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateMember(TeamMember teamMember, Guid teamGuid)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Backend/TeamsApi/Teams.Service/Queries/GetTeam/GetTeamQuery.cs
@@ -0,0 +1,9 @@
+﻿using Teams.Service.Abstractions;
+using Teams.Service.Dto;
+
+namespace Teams.Service.Queries.GetTeam;
+
+public class GetTeamQuery : IQuery<TeamDto>
+{
+    public Guid Guid { get; set; }
+}
