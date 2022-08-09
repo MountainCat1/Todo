3f3831d5cbfd3e8e5da784bfc84e98db49f94e08
MountainCat <tomasz.banas123@gmail.com>
2022-08-09 17:27:39 +0200

    Changed naming, added entity

---
+++ Backend/TeamMembership/TeamMemberships.Infrastructure/Data/TeamMembershipDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamMemberships.Domain.Entities;
+
+namespace TeamMemberships.Infrastructure.Data;
+
+public class TeamMembershipDbContext : DbContext
+{
+    public TeamMembershipDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<TeamMembership> TeamMemberships { get; set; }
+}
