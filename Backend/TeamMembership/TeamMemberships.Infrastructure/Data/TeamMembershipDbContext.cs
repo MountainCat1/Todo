7f48383b4c1c3c27aac209dae67ce93e3074a9a0
MountainCat <tomasz.banas123@gmail.com>
2022-08-10 18:25:09 +0200

    Added composite key

--- Backend/TeamMembership/TeamMemberships.Infrastructure/Data/TeamMembershipDbContext.cs
+++ Backend/TeamMembership/TeamMemberships.Infrastructure/Data/TeamMembershipDbContext.cs
@@ -8,6 +8,12 @@ public class TeamMembershipDbContext : DbContext
     public TeamMembershipDbContext(DbContextOptions options) : base(options)
     {
     }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TeamMembership>()
+            .HasKey(entity => new { entity.TeamGuid, entity.UserGuid });
+    }
 
     public DbSet<TeamMembership> TeamMemberships { get; set; }
 }
