b638ab3a6d5a3992469848092128b1555799c1be
MountainCat <tomasz.banas123@gmail.com>
2022-08-10 17:53:59 +0200

    Added queries

---
+++ Backend/TeamMembership/TeamMemberships.Service/Queries/GetMembership/GetMembershipQuery.cs
@@ -0,0 +1,10 @@
+﻿using TeamMemberships.Service.Abstractions;
+using TeamMemberships.Service.Dto;
+
+namespace TeamMemberships.Service.Queries.GetMembership;
+
+public class GetMembershipQuery : IQuery<TeamMembershipDto>
+{
+    public Guid TeamGuid { get; set; }
+    public Guid UserGuid { get; set; }
+}
