c34b141c54b1899cbdf6e91fe94e01d3b635a703
MountainCat <tomasz.banas123@gmail.com>
2022-08-11 17:04:42 +0200

    Added infrastructure layer

---
+++ Backend/Users/Users.Infrastructure/Repositories/TeamMembershipRepository.cs
@@ -0,0 +1,13 @@
+﻿using Users.Domain.Entities;
+using Users.Domain.Repositories;
+using Users.Infrastructure.Data;
+using Users.Infrastructure.Generics;
+
+namespace Users.Infrastructure.Repositories;
+
+public class UserRepository : Repository<User, UserDbContext>, IUserRepository
+{
+    public UserRepository(UserDbContext dbContext) : base(dbContext)
+    {
+    }
+}
