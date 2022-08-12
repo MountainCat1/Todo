c7ffb1722d27a43a77f537a353ebc5f36d3c89fd
MountainCat <tomasz.banas123@gmail.com>
2022-08-12 14:52:29 +0200

    added infrastructure layer

---
+++ Backend/AuthenticationApi/Authentication.Infrastructure/Data/AccountDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Authentication.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.Infrastructure.Data;
+
+public class AccountDbContext : DbContext
+{
+    public AccountDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+
+    public DbSet<Account> Accounts { get; set; }
+}
---
+++ Backend/AuthenticationApi/Authentication.Infrastructure/Repositories/TeamMembershipRepository.cs
@@ -0,0 +1,13 @@
+﻿using Authentication.Domain.Entities;
+using Authentication.Domain.Repositories;
+using Authentication.Infrastructure.Data;
+using Authentication.Infrastructure.Generics;
+
+namespace Authentication.Infrastructure.Repositories;
+
+public class AccountRepository : Repository<Account, AccountDbContext>, IAccountRepository
+{
+    public AccountRepository(AccountDbContext dbContext) : base(dbContext)
+    {
+    }
+}
