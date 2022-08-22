d436d8bfb9ab9f1121018ff6b09065f83917d518
MountainCat <tomasz.banas123@gmail.com>
2022-08-23 01:05:46 +0200

    added Seeding role table

--- Backend/TeamMembershipApi/TeamMemberships.Infrastructure/Data/TeamMembershipDbContext.cs
+++ Backend/TeamMembershipApi/TeamMemberships.Infrastructure/Data/TeamMembershipDbContext.cs
@@ -13,7 +13,15 @@ public class TeamMembershipDbContext : DbContext
     {
         modelBuilder.Entity<TeamMembership>()
             .HasKey(entity => new { entity.TeamGuid, UserGuid = entity.AccountGuid });
-    }
 
+        // Seeding role table
+        modelBuilder.Entity<Role>().HasData(
+            new Role { Name = "Admin" }, 
+            new Role { Name = "Moderator" }, 
+            new Role { Name = "Member" });
+    }
+    
     public DbSet<TeamMembership> TeamMemberships { get; set; }
+
+    public DbSet<Role> Roles { get; set; }
 }
