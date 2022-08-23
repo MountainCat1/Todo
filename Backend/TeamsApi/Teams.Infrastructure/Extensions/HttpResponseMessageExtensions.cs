a8ad4b7b99687a666ee3f8578ce31043e93a9c9c
MountainCat <tomasz.banas123@gmail.com>
2022-08-23 02:52:53 +0200

    added basic TodoClient.cs

---
+++ Backend/TeamsApi/Teams.Infrastructure/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http.Json;
+
+namespace Teams.Infrastructure.Extensions;
+
+public static class HttpResponseMessageExtensions
+{
+    public static async Task<T> GetObject<T>(this HttpResponseMessage httpResponseMessage)
+    {
+        return await httpResponseMessage.Content.ReadFromJsonAsync<T>();
+    }
+}
---
+++ Backend/TeamsApi/Teams.Infrastructure/HttpClients/TodoClient.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http.Json;
+using System.Runtime.Serialization;
+using Teams.Infrastructure.Dto;
+
+namespace Teams.Infrastructure.HttpClients;
+
+public interface ITodoClient
+{
+}
+
+public class TodoClient : ITodoClient
+{
+    private readonly HttpClient _httpClient;
+
+    private readonly string _getTeamTodosEndpoint = "getAll?teamGuid={0}";
+    
+    public TodoClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<ICollection<TodoDto>> GetTeamTodos(Guid teamGuid)
+    {
+        var endpoint = string.Format(_getTeamTodosEndpoint, teamGuid);
+
+        var endpointUri = new Uri(endpoint, UriKind.Relative);
+
+        var response = await _httpClient.GetAsync(endpointUri);
+
+        if (!response.IsSuccessStatusCode)
+            throw new HttpRequestException(response.ReasonPhrase);
+
+        return await response.Content.ReadFromJsonAsync<ICollection<TodoDto>>() 
+               ?? throw new SerializationException();
+    }
+
+}
