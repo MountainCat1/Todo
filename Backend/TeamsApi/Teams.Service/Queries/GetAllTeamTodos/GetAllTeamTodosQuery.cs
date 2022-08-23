5fccd0804ddd814f1c6c267a87255e9cfa594019
MountainCat <tomasz.banas123@gmail.com>
2022-08-23 03:01:49 +0200

    added query

---
+++ Backend/TeamsApi/Teams.Service/Queries/GetAllTeamTodos/GetAllTeamTodosQuery.cs
@@ -0,0 +1,9 @@
+﻿using Teams.Infrastructure.Dto;
+using Teams.Service.Abstractions;
+
+namespace Teams.Service.Queries.GetAllTeamTodos;
+
+public class GetAllTeamTodosQuery : IQuery<ICollection<TodoDto>>
+{
+    public Guid TeamGuid { get; set; }
+}
