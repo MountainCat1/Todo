bb645d9bdc27ee9a45722fd04ae7dbafaa261f9c
MountainCat <tomasz.banas123@gmail.com>
2022-08-24 04:10:16 +0200

    added initial code for resource based authorization

---
+++ Backend/TeamsApi/Teams.Api/AuthorizationHandlers/TeamAuthorizationHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Teams.Domain.Entities;
+using Teams.Infrastructure.HttpClients;
+
+namespace Teams.Api.AuthorizationHandlers;
+
+public class TeamAuthorizationHandler : AuthorizationHandler<OperationAuthorizationRequirement, Team>
+{
+    private readonly IMembershipClient _membershipClient;
+
+    public TeamAuthorizationHandler(IMembershipClient membershipClient)
+    {
+        _membershipClient = membershipClient;
+    }
+
+    protected override async Task HandleRequirementAsync(
+        AuthorizationHandlerContext context,
+        OperationAuthorizationRequirement requirement,
+        Team resource)
+    {
+        // TODO, changed ClaimTypes.NameIdentifier to ClaimTypes.Sid
+        var accountGuid = Guid.Parse(context.User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value);
+        var membership = await _membershipClient.GetMembershipAsync(resource.Guid, accountGuid);
+        
+        if (membership != null) // TODO // && requirement.Name == ResourceOperation.Read)
+        {
+            context.Succeed(requirement);
+        }
+    }
+}
---
+++ Backend/TeamsApi/Teams.Infrastructure/HttpClients/MembershipClient.cs
@@ -0,0 +1,21 @@
+﻿using Teams.Infrastructure.Dto;
+
+namespace Teams.Infrastructure.HttpClients;
+
+public interface IMembershipClient
+{
+    public Task<MembershipDto?> GetMembershipAsync(Guid teamGuid, Guid accountGuid);
+}
+
+public class MembershipClient : IMembershipClient
+{
+    // TODO make a client responsible for querying data from membership microservice
+    
+    public async Task<MembershipDto?> GetMembershipAsync(Guid teamGuid, Guid accountGuid)
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+}
+
+
