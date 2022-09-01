fb4a61f4e782940128ca3e15ff76b167d4442f9b
MountainCat <tomasz.banas123@gmail.com>
2022-09-01 02:58:09 +0200

    Added basic layout of main panel

---
+++ Frontend/todo-front-end/src/components/MainPanel/MainPanel.tsx
@@ -0,0 +1,16 @@
+import './MainPanel.css';
+
+export default function () {
+    return (
+        <div className='Main-panel Panel'>
+            <div className='top-panel'>
+
+            </div>
+            <div className='left-panel'>
+
+            </div>
+            <div className='center-panel'>
+
+            </div>
+        </div>)
+}
