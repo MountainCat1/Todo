e07cd21b379b0bd0160e3d68eb19efb87430472d
MountainCat <tomasz.banas123@gmail.com>
2022-09-03 23:59:58 +0200

    added user api to api gateway swagger

--- Backend/UsersApi/Users.Api/Program.cs
+++ Backend/UsersApi/Users.Api/Program.cs
@@ -8,6 +8,7 @@ using MediatR.Extensions.FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Users.Api.Configuration;
 using Users.Api.Middleware;
 using Users.Domain.Repositories;
@@ -37,7 +38,29 @@ services.AddControllers();
 services.AddEndpointsApiExplorer();
 services.AddHttpsRedirection(options =>
     options.HttpsPort = configuration.GetValue<int>("HTTPS_PORT"));
-services.AddSwaggerGen();
+services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 services.AddLogging(options =>
 {
     options.AddConsole();
