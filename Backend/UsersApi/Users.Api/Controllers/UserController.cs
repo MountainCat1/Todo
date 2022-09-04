93b7aaa66e34ceb631021ce5901b63b814c18f15
MountainCat <tomasz.banas123@gmail.com>
2022-09-04 18:05:28 +0200

    added working get my user data endpoint

--- Backend/UsersApi/Users.Api/Controllers/UserController.cs
+++ Backend/UsersApi/Users.Api/Controllers/UserController.cs
@@ -1,11 +1,8 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Users.Service.Commands.CreateUser;
-using Users.Service.Commands.DeleteUser;
-using Users.Service.Commands.UpdateUser;
-using Users.Service.Dto;
 using Users.Service.Query.GetUser;
-using Users.Service.Query.GetUsers;
+using Users.Service.Query.GetUserFromClaims;
 
 namespace Users.Api.Controllers;
 
@@ -15,7 +12,7 @@ namespace Users.Api.Controllers;
 public class UserController : Controller
 {
     private readonly IMediator _mediator;
-
+    
     public UserController(IMediator mediator)
     {
         _mediator = mediator;
@@ -27,6 +24,19 @@ public class UserController : Controller
     public async Task<IActionResult> Get([FromQuery] Guid guid)
     {
         var query = new GetUserQuery(guid);
+
+        var queryResult = await _mediator.Send(query);
+
+        return Ok(queryResult);
+    }
+    
+    [Authorize]
+    [HttpGet("me")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetMyUserData()
+    {
+        var query = new GetUserFromClaimsQuery(User);
 
         var queryResult = await _mediator.Send(query);
 
