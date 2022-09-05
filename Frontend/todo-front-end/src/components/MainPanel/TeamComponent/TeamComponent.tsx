53ebc517efc279041fa93d1a181f03252fd76e35
MountainCat <tomasz.banas123@gmail.com>
2022-09-05 02:47:04 +0200

    created create team popup base

--- Frontend/todo-front-end/src/components/MainPanel/TeamComponent/TeamComponent.tsx
+++ Frontend/todo-front-end/src/components/MainPanel/TeamComponent/TeamComponent.tsx
@@ -1,12 +1,32 @@
+import Popup from "components/Popup/Popup";
+import {useState} from "react";
 
+type TeamComponentState = {
+    showFormPopup: boolean
+}
 
+export default function TeamComponent() {
+    const [state, setState] = useState<TeamComponentState>({
+        showFormPopup: false
+    });
 
-export default function TeamComponent(){
+    const handleCreateTeamButton = () => {
+        setState({
+            ...state,
+            showFormPopup: !state.showFormPopup
+        })
+    }
 
-
-    return(<>
-        <button className='button button-size-small'>
+    return (<>
+        <button className='button button-size-small' onClick={handleCreateTeamButton}>
             Create Team
         </button>
+
+        {state.showFormPopup
+            ? <Popup>
+                XDD
+            </Popup>
+            : <></>
+        }
     </>)
 }
