63f3c0ca1b5f46accc79d96423979745a4649c00
MountainCat <tomasz.banas123@gmail.com>
2022-09-07 21:36:55 +0200

    useApiCreateTeam now returns void

--- Frontend/todo-front-end/src/api/teamApi.ts
+++ Frontend/todo-front-end/src/api/teamApi.ts
@@ -9,22 +9,16 @@ export type CreateTeamDto = {
 export function useApiCreateTeam() {
     const apiPost = useApiPost();
 
-    return( async (createDto : CreateTeamDto) : Promise<CreateTeamDto | null> => {
+    return( async (createDto : CreateTeamDto) : Promise<void> => {
         let team: CreateTeamDto | null = null;
 
         apiPost('/team/create', createDto)
             .then(response => {
                 if (!response.ok)
                     throw new Error(response.statusText);
-                return response.json();
-            })
-            .then(responseJson => {
-                team = responseJson as CreateTeamDto;
             })
             .catch((reason) => {
                 console.error(reason);
             });
-
-        return team;
     })
 }
